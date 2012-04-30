c6fdd88472892d88aa5bb1215b6cd4f267888ab4
Espen A. Kristiansen <post@espenak.net>
2012-04-30 15:43:16 +0200

    decorators: Commented out ec2instance because it must be lazy to work.

--- awsfabrictasks/decorators.py
+++ awsfabrictasks/decorators.py
@@ -1,28 +1,28 @@
-from functools import wraps
-from fabric.decorators import _wrap_as_new
-from fabric.api import settings
+#from functools import wraps
+#from fabric.decorators import _wrap_as_new
+#from fabric.api import settings
 
-from .ec2 import Ec2InstanceWrapper
+#from .ec2.api import Ec2InstanceWrapper
 
 
-def ec2instance(nametag=None, instanceid=None):
-    """
-    Wraps the decorated function to execute as if it had been invoked with
-    ``--ec2names`` or ``--ec2ids``.
-    """
-    if instanceid:
-        instance = Ec2InstanceWrapper.get_by_instanceid(instanceid)
-    elif nametag:
-        instance = Ec2InstanceWrapper.get_by_nametag(nametag)
-    else:
-        raise ValueError('nametag or instanceid must be supplied.')
+#def ec2instance(nametag=None, instanceid=None):
+    #"""
+    #Wraps the decorated function to execute as if it had been invoked with
+    #``--ec2names`` or ``--ec2ids``.
+    #"""
+    #if instanceid:
+        #instance = Ec2InstanceWrapper.get_by_instanceid(instanceid)
+    #elif nametag:
+        #instance = Ec2InstanceWrapper.get_by_nametag(nametag)
+    #else:
+        #raise ValueError('nametag or instanceid must be supplied.')
 
-    ssh_uri = instance.get_ssh_uri()
-    key_filename = instance.get_ssh_key_filename()
-    def outer(func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            with settings(key_filename=key_filename, host_string=ssh_uri):
-                return func(*args, **kwargs)
-        return _wrap_as_new(func, inner)
-    return outer
+    #ssh_uri = instance.get_ssh_uri()
+    #key_filename = instance.get_ssh_key_filename()
+    #def outer(func):
+        #@wraps(func)
+        #def inner(*args, **kwargs):
+            #with settings(key_filename=key_filename, host_string=ssh_uri):
+                #return func(*args, **kwargs)
+        #return _wrap_as_new(func, inner)
+    #return outer
