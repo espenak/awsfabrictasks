08838fc8365947b091f2f21c8ea9ae452be3da2f
Espen A. Kristiansen <post@espenak.net>
2012-04-30 15:44:11 +0200

    Removed unused function.

--- awsfabrictasks/utils.py
+++ awsfabrictasks/utils.py
@@ -1,6 +1,4 @@
-from fabric.api import put, env, sudo
-
-
+from fabric.api import put, sudo
 
 
 def upload_config(local_path, remote_path, owner=None, mode=None):
@@ -9,12 +7,3 @@ def upload_config(local_path, remote_path, owner=None, mode=None):
         sudo('chown {owner} {remote_path}'.format(**vars()))
     if mode:
         sudo('chmod {mode} {remote_path}'.format(**vars()))
-
-
-def format_keypairs_for_ssh_options():
-    """
-    Get ssh/rsync formatted ``env.key_filename``. E.g.::
-
-        "-i /path/to/key1.pem -i /path/to/key2.pem"
-    """
-    return ' '.join(['-i {0}'.format(key) for key in env.key_filename])
