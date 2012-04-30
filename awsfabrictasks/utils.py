5ce1d49c703ad39e7f8ab365fac635139ca9c59a
Espen A. Kristiansen <post@espenak.net>
2012-04-30 21:04:29 +0200

    utils: Explicit sudo, and created sudo_upload_dir.

--- awsfabrictasks/utils.py
+++ awsfabrictasks/utils.py
@@ -1,9 +1,48 @@
 from fabric.api import put, sudo
+from os import walk
+from os.path import relpath, join
 
 
-def upload_config(local_path, remote_path, owner=None, mode=None):
-    put(local_path, remote_path, use_sudo=True)
+def sudo_chown(remote_path, owner):
+    sudo('chown {owner} {remote_path}'.format(**vars()))
+
+def sudo_chmod(remote_path, mode):
+    sudo('chmod {mode} {remote_path}'.format(**vars()))
+
+def sudo_chattr(remote_path, owner=None, mode=None):
     if owner:
-        sudo('chown {owner} {remote_path}'.format(**vars()))
+        sudo_chown(remote_path, owner)
     if mode:
-        sudo('chmod {mode} {remote_path}'.format(**vars()))
+        sudo_chmod(remote_path, mode)
+
+def sudo_upload_file(local_path, remote_path, **chattr_kw):
+    put(local_path, remote_path, use_sudo=True)
+    sudo_chattr(remote_path, **chattr_kw)
+
+
+def sudo_mkdir_p(remote_path, **chattr_kw):
+    """
+    ``sudo mkdir -p <remote_path>`` followed by :func:`sudo_chattr`(remote_path, **chattr_kw).
+    """
+    sudo('mkdir -p {remote_path}'.format(**vars()))
+    sudo_chattr(remote_path, **chattr_kw)
+
+
+def sudo_upload_dir(local_dir, remote_dir, **chattr_kw):
+    """
+    Upload all files and directories in ``local_dir`` to ``remote_dir``.
+    Directories are created with :func:`sudo_mkdir_p` and files are uploaded
+    with :func:`sudo_upload_file`. ``chattr_kw`` is forwarded in both cases.
+    """
+    for local_dirpath, dirnames, filenames in walk(local_dir):
+        remote_dirpath = remote_dir
+        rel = relpath(local_dirpath, local_dir)
+        if rel != '.':
+            remote_dirpath = join(remote_dir, rel)
+        #print local_dirpath, '-->', remote_dirpath
+        sudo_mkdir_p(remote_dirpath, **chattr_kw)
+        for filename in filenames:
+            local_filepath = join(local_dirpath, filename)
+            remote_filepath = join(remote_dirpath, filename)
+            #print local_filepath, '-->', remote_filepath
+            sudo_upload_file(local_filepath, remote_filepath, **chattr_kw)
