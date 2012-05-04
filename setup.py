994af27b60b378281aa23e5191ce734813b22811
Espen A. Kristiansen <post@espenak.net>
2012-05-04 22:43:42 +0200

    Add README.rst to MANIFEST.in.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name = 'awsfabrictasks',
       description = 'Fabric tasks for Amazon Web Services with some extra utilities for Ubuntu.',
-      version = '1.0',
+      version = '1.0.1',
       license = 'BSD',
       url = 'https://github.com/espenak/awsfabrictasks',
       author = 'Espen Angell Kristiansen',
