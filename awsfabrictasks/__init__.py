a4552c3f1ed32e534c9306f34e036578d5cd281f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-06-10 17:16:29 -0400

    Adds expand_roledefs().  Use instance tags to build your roles instead.

--- awsfabrictasks/__init__.py
+++ awsfabrictasks/__init__.py
@@ -1,1 +1,13 @@
+from fabric.api import env
+from .ec2.api import Ec2InstanceWrapper
+
 version = '1.1.1'
+
+
+def expand_roledefs():
+    for k, v in env.roledefs.iteritems():
+        if isinstance(v, dict):
+            if 'ec2:tagged' in v:
+                region = v['ec2:tagged'].pop('region') if 'region' in v['ec2:tagged'] else None
+                instancewrappers = Ec2InstanceWrapper.get_by_tagvalue(v['ec2:tagged'], region)
+                env.roledefs[k] = [instancewrapper['public_dns_name'] for instancewrapper in instancewrappers]
